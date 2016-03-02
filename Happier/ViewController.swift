3c5e712bf7965e8d2492d118b44168c7f56d431c
Vicente Noguez Salazar <vicente@betelgeuse.local>
2016-03-01 18:49:27 -0600

    Innitial commit

--- Happier/ViewController.swift
+++ Happier/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var positiveMessage: UILabel!
+    
+    let colors = Colors()
+    let phrases = Data()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func givePositiveMessage(sender : AnyObject) {
+        positiveMessage.text = phrases.returnHappyPhrase();
+        let randomColor = colors.returnRandomColor()
+        view.backgroundColor = randomColor
+        view.tintColor = randomColor
+    }
 
 }
 
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+// Playground: concept review
+
+import UIKit
+
+// Variables and constants
+let name = "Uinseann"
+var age  = 28
+
+// Arrays
+var numbers = [8, 8, 5, 1, 7]
+numbers.append(6)
+numbers[5]
+
+// Optionals
+var surname : String? = "De Gerderest"
+surname = nil
+
+let color = UIColor(red: 0/255, green: 149/255, blue: 48/255, alpha: 1)
+
+arc4random() % 100
+
+/*
+let colors = [UIColor(red: 210/255, green: 90/255, blue: 45/255, alpha: 1),
+UIColor(red: 40/255, green: 170/255, blue: 45/255, alpha: 1),
+UIColor(red: 3/255, green: 100/255, blue: 90/255, alpha: 1),
+UIColor(red: 210/255, green: 190/255, blue: 5/255, alpha: 1),
+UIColor(red: 120/255, green: 120/255, blue: 50/255, alpha: 1),
+UIColor(red: 130/255, green: 80/255, blue: 90/255, alpha: 1),
+UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1),
+UIColor(red: 3/255, green: 50/255, blue: 90/255, alpha: 1)]
+*/
